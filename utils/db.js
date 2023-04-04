5e2fc01b6e233611048f1752984d11dcb090d78b
Samuel <samueldesta323@gmail.com>
2023-04-04 12:24:19 +0300

    added documentation file

--- utils/db.js
+++ utils/db.js
@@ -1,6 +1,13 @@
 const { MongoClient } = require('mongodb');
 
+/**
+ * DBClient - class for creating and managing connection to mongo db
+ */
 class DBClient {
+  /**
+     * constructor for creating and verifying mongodb connection
+     *
+      */
   constructor () {
     this.host = process.env.DB_HOST || 'localhost';
     this.port = process.env.DB_PORT || 27017;
@@ -16,16 +23,28 @@ class DBClient {
       });
   }
 
+  /**
+     * checks whether connection was created or not
+     * @returns {boolean|*}
+     */
   isAlive () {
     return this.connected;
   }
 
+  /**
+     * counts the number of users in file_manager db
+     * @returns {Promise<*>}
+     */
   async nbUsers () {
     this.db = this.client.db(this.database);
     const users = this.db.collection('users');
     return await users.countDocuments({}, { hint: '_id_' });
   }
 
+  /**
+     * counts the number of files in the file_manager db
+     * @returns {Promise<*>}
+     */
   async nbFiles () {
     this.db = this.client.db(this.database);
     const files = this.db.collection('files');
